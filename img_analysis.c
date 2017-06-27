9b3dcbd562ce9ba84ac8639e011d113084d4e374
DH Kim <inerplat@gmail.com>
2017-06-27 11:48:21 +0900

    각 함수별 틀 작성

---
+++ img_analysis.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <math.h>
+#include "img_analysis.h"
+
+int MCU_analysis(U16* buf)
+{
+  return 1;
+}
